e83720f1a57c788748c036c26b896c5357773597
Christopher Tavolazzi <ctavolazzi@gmail.com>
2022-07-15 14:02:03 -0700

    Adds ability for Warriors to pick up Items
    Adds simple logic to the main file that allows Warriors to pick up Items and store them in their inventory.
    
    In future versions, this logic should be extracted to separate files and imported into the main Arena file.

--- arena.py
+++ arena.py
@@ -5,21 +5,36 @@ from fight import fight
 
 
 # # Initialize the simulation
-warrior1 = Warrior("Ognar", 100, 5, [Items[0]])
+
+# warrior1 = Warrior("Ognar", 100, 5, [Items[0]])
 # print("Warrior 1: ")
 # print(warrior1)
 # for key, value in warrior1.items():
 #   print(key, ' : ', value)
 
-warrior2 = Warrior("Glubart", 100, 6, [Items[1]])
+# warrior2 = Warrior("Glubart", 100, 6, [Items[1]])
 # print("Warrior 2 : ")
 # print(warrior2)
 
 
 # # Set up environmental variables
 warriors = []
-warriors.append(warrior1)
-warriors.append(warrior2)
+number_of_warriors = input("How many Warriors?: ")
+
+count = 0
+
+def item_check(Items):
+  if len(Items) > 0:
+    selected_item = Items.pop()
+    return [selected_item]
+  else:
+    return []
+
+while count < int(number_of_warriors):
+  warriors.append(Warrior(input("Name: "), input("HP: "), input("AP: "), item_check(Items)))
+  count+=1
+# warriors.append(warrior1)
+# warriors.append(warrior2)
 # print("Warriors: ")
 # for Warrior in warriors:
 #   print(Warrior)
@@ -31,8 +46,14 @@ print("Warriors: ")
 for Warrior in warriors:
   print(Warrior)
 # print("(readout of simulation parameters)")
-proceed_with_simulation = input("Y, N? ").lower()
+proceed_with_simulation = input("Y, N? ").upper()
+
+if proceed_with_simulation == "Y":
+  print("Simulation Complete")
+elif proceed_with_simulation == "N":
+  print("Simulation Cancelled")
+else:
+  print("Unrecognized Input...Simulation Cancelled")
 
 # Run simulation
-fight_result = fight(warrior1, warrior2)
-print(fight_result)
+# fight_result = fight(warrior1, warrior2)
