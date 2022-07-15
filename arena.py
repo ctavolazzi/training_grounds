cdf5408185a0e24df5a35cfd4149034d25a0f609
Christopher Tavolazzi <ctavolazzi@gmail.com>
2022-07-15 13:42:17 -0700

    Remove debugging print statements and move environment printout to the final check section

--- arena.py
+++ arena.py
@@ -1,34 +1,36 @@
-# Import functions, classes, items, etc
+# # Import functions, classes, items, etc
 from Warrior import Warrior
 from Items import Items
 from fight import fight
 
-# Initialize the simulation
+
+# # Initialize the simulation
 warrior1 = Warrior("Ognar", 100, 5, [Items[0]])
-print("Warrior 1: ")
-print(warrior1)
+# print("Warrior 1: ")
+# print(warrior1)
 # for key, value in warrior1.items():
 #   print(key, ' : ', value)
 
 warrior2 = Warrior("Glubart", 100, 6, [Items[1]])
-print("Warrior 2 : ")
-print(warrior2)
+# print("Warrior 2 : ")
+# print(warrior2)
 
 
-# Set up environmental variables
+# # Set up environmental variables
 warriors = []
-
 warriors.append(warrior1)
 warriors.append(warrior2)
+# print("Warriors: ")
+# for Warrior in warriors:
+#   print(Warrior)
 
+
+# # Perform final check
+print("Does this look right?")
 print("Warriors: ")
 for Warrior in warriors:
   print(Warrior)
-
-
-# Perform final check
-print("Does this look right?")
-print("(readout of simulation parameters)")
+# print("(readout of simulation parameters)")
 proceed_with_simulation = input("Y, N? ").lower()
 
 # Run simulation
